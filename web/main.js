7c771fabc268acecb90ee9f749e266e98b42fc4a
Brian <brayomumo5@gmail.com>
2019-10-14 18:31:42 +0300

    Add client server locator

--- web/main.js
+++ web/main.js
@@ -31,7 +31,25 @@ var projectPoint = function(point) {
     }
     return pointCache[key];
 }
+// Geo locating a client
 
+if ("geolocation" in navigator) {
+  // check if geolocation is supported/enabled on current browser
+  navigator.geolocation.getCurrentPosition(
+   function success(position) {
+     // for when getting location is a success
+     console.log('latitude', position.coords.latitude, 
+                 'longitude', position.coords.longitude);
+   },
+  function error(error_message) {
+    // for when getting location results in an error
+    console.error('An error has occured while retrieving location', error_message)
+  }  
+)} else {
+  // geolocation is not supported
+  // get your location some other way
+  console.log('geolocation is not enabled on this browser')
+}
 var color = d3.scale.category20();
 
 var line = d3.svg.line()
