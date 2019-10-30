7aa5d9d413521d88ead0b3c81ebbdd64efb5adb5
Brian <brayomumo5@gmail.com>
2019-10-30 15:43:54 +0300

    Url to get the data from database

--- application/urls.py
+++ application/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="home"),
-    url(r'^getdata/', views.getData, name="home"),
+    url(r'^getdata', views.getData, name="home"),
 ]
