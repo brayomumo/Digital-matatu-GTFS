84cec87f8200aa611551129f10a233f1efb9f96d
Brian <brayomumo5@gmail.com>
2019-10-30 15:43:13 +0300

    All data query

--- application/models.py
+++ application/models.py
@@ -8,3 +8,33 @@ class datatable(models.Model):
     choice_text = models.CharField( max_length= 50)
     question_text = models.CharField( max_length= 200)
     total_count = models.IntegerField( )
+
+    def __str__(self):
+        return self.choice_id
+
+class allData(models.Model):
+    choice_id = models.IntegerField()
+    choice_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
+    question_id = models.IntegerField()
+
+
+    def __str__(self):
+        return self.choice_id
+
+    
+
+class areaQuestionData(models.Model):
+    Question_text = models.CharField(max_length=200)
+    Choice_text = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.Choice_text
+
+class finalData:
+    def __init__(self,name,coordinates,radius):
+        self.name = name
+        self.coordinates = coordinates
+        self.radius = radius
+
