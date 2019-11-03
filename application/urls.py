8172d7bbad2f61060641d1b45857a206e6069f2c
Brian <brayomumo5@gmail.com>
2019-11-03 15:24:09 +0300

    Statisc files

--- application/urls.py
+++ application/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name="home"),
@@ -7,3 +9,5 @@ urlpatterns = [
     url(r'^ResponceOnClick/(\d+)', views.ResponceOnClick, name="ResponceOnClick"),
     url(r'^response//$', views.getResponse, name='getResponse'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
