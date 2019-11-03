0342151aea49a3f1db46c1f5097bc8071457f3c0
Brian <brayomumo5@gmail.com>
2019-11-03 14:42:10 +0300

    Create json response url

--- application/urls.py
+++ application/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="home"),
-    url(r'^getdata', views.getData, name="home"),
+    url(r'^getdata', views.getData, name="getData"),
+    url(r'^ResponceOnClick/(\d+)', views.ResponceOnClick, name="ResponceOnClick"),
+    url(r'^response//$', views.getResponse, name='getResponse'),
 ]
